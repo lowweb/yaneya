778248a724aa943f3bf4674b0831a2d7edc60784
lowweb <looper@MacBook-Pro-looper.local>
2021-09-09 23:54:41 +1000

    readme

--- 02/playground.js
+++ 02/playground.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var sam = [];
 
 ((global) => {
     const timeout = 20;
@@ -55,7 +54,6 @@ const input = Folder([
 // проверка решения
 solution(input).then(result => {
     console.log (result)
-    console.log(sam)
     const answer = ['ffffile', 'ffiillee', 'ffiillee', 'fiiile', 'filllle'];
     const isEqual = String(answer) === String(result);
 
@@ -69,42 +67,15 @@ solution(input).then(result => {
 
 
 async function solution(input) {
+    global.lx = 0;
     
-    // ... решение задачи
-    
-    // for (let sub of Object.values(input)) {
-    //     console.log(typeof sub)
-    //   }
-    // for (let item in input) {
-    //     // console.log(typeof item)
-    //     item.read(2, (file) => console.log(typeof file));
-    // }
-
-    // console.log(input['file'])
-    // пример вызова read
-    // input.read(2, (folder) => console.log(folder.read(0, (folder)=> console.log(folder))));
-    // let i=0;
-    // for (let i =0; i<=5; i++) {
-    //    return  i += input.read(1, (file) => file);
-    // }
-    // console.log(i)
-    // return countElement
-    // input.read(2, (file) => console.log(file));
-    // пример вызова size
-    // input.size((size) => console.log(size));
-
-    // input.read(0, (file) => {
-    //     for (let subdep in Object.values(file)) {
-    //         console.log(subdep)
-    //     }
-    // });
     input.size((size) => {
             for (let index = 0; index < size; index++)
             {
                 input.read(index,(file) => {
                     if (typeof file === 'string') {
                         // console.log(file);
-                        sam.push(file);
+                        global.lx = 3;
                     }
     
                     if (typeof file === 'object' && file != null && Object.keys(file).length != 0) {
@@ -115,10 +86,7 @@ async function solution(input) {
        
             }
         });
-    
-    
-    // return 'dsadas'
-    // console.log(sam)
+     console.log(global.lx)   
     // return ['ffffile', 'ffiillee', 'ffiillee', 'fiiile', 'filllle']
 
 }
